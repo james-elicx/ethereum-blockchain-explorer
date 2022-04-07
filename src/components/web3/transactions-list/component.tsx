0136d31af7f5e7addb347104c183d9eb49d3f9d5
James <james@eli.cx>
2022-04-07 02:34:49 +0100

    fix: bug importing from moment

--- src/components/web3/transactions-list/component.tsx
+++ src/components/web3/transactions-list/component.tsx
@@ -1,4 +1,4 @@
-import { now } from 'moment';
+import moment from 'moment';
 import { FlexBox } from '../../../components';
 import { useInfura } from '../../../contexts';
 import { trimAddress } from '../../../utils';
@@ -10,7 +10,8 @@ export const Transactions = () => {
     <FlexBox direction="col">
       {transactions
         .sort((a, b) => {
-          return !b.timestamp || !a.timestamp ? now() : b.timestamp - a.timestamp;
+          // eslint-disable-next-line import/no-named-as-default-member
+          return !b.timestamp || !a.timestamp ? moment.now() : b.timestamp - a.timestamp;
         })
         .slice(1)
         .slice(-5)
